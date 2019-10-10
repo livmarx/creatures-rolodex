2b72bb1e1a9b81d13d3db1421d1476253f02e876
Liv Marx <oehrenmarks@gmail.com>
2019-10-10 13:14:49 -0400

    Updates

---
+++ precache-manifest.fd9596d4140d7b5e4ba422daa82bb7ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f2aa99a7b486f6dfed80eb22a2f144",
+    "url": "/creatures-rolodex/index.html"
+  },
+  {
+    "revision": "ee70c4adb94c3e0ac5db",
+    "url": "/creatures-rolodex/static/css/main.f1c16b79.chunk.css"
+  },
+  {
+    "revision": "5f195867244b9c7159dc",
+    "url": "/creatures-rolodex/static/js/2.69c444ea.chunk.js"
+  },
+  {
+    "revision": "ee70c4adb94c3e0ac5db",
+    "url": "/creatures-rolodex/static/js/main.9b71276b.chunk.js"
+  },
+  {
+    "revision": "01e0a31507fe74bab760",
+    "url": "/creatures-rolodex/static/js/runtime-main.2e001d9a.js"
+  }
+]);
